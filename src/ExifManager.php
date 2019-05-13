b9e39dbba9af80f668720611a716b860424e13a2
Florent Brusciano <fbrusciano@webedia.fr>
2019-05-13 14:30:07 +0200

    nice version

--- src/ExifManager.php
+++ src/ExifManager.php
@@ -17,8 +17,18 @@ class ExifManager extends AbstractManager {
         return array_key_exists($tag, $this->meta) ? $this->meta[$tag] : false;
     }
 
+    /**
+     * Not implemented
+     * 
+     * PEL(PHP Exif Library). A library for reading and writing Exif headers in JPEG and TIFF images using PHP.
+     * The PHP JPEG Metadata Toolkit. Allows reading, writing and display of the following JPEG metadata formats: EXIF 2.2, XMP / RDF, IPTC-NAA IIM 4.1 ect
+     * 
+     * 
+     * @param \ImgMeta\ImgWriter $writer
+     * @return boolean
+     */
     public function write(ImgWriter $writer) {
         return false;
     }
-    
+
 }
