41c5f74b3bb1262735ed953bc3c2b2a7be4641f6
Florent Brusciano <fbrusciano@webedia.fr>
2019-05-16 08:56:18 +0200

    testing reader

--- src/ExifReader.php
+++ src/ExifReader.php
@@ -17,15 +17,19 @@ abstract class ExifReader implements ImgReader {
 
     public static function getReader($stream) {
 
+        $reader = null;
+
         if (filter_var($stream, FILTER_VALIDATE_URL)) {
-            return new ExifUrlReader($stream);
+            $reader = new ExifUrlReader($stream);
         } else if (exif_imagetype('data://image/jpeg;base64,' . base64_encode($stream)) !== false) {
-            return new ExifStringReader($stream);
+            $reader = new ExifStringReader($stream);
         } else if (file_exists($stream)) {
-            return new ExifFileReader($stream);
-        } else {
-            return null;
+            $reader = new ExifFileReader($stream);
         }
+        
+        dd($reader);
+        
+        return $reader;
     }
 
     public abstract function read();
