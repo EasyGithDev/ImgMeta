2e7724a3f9335b0f7ef90f6e25e4677013914ff2
Florent Brusciano <fbrusciano@webedia.fr>
2019-05-24 12:38:36 +0200

    adding ref

--- src/Gps/Point.php
+++ src/Gps/Point.php
@@ -18,18 +18,40 @@ class Point {
     protected $deg = 0;
     protected $min = 0;
     protected $sec = 0;
+    protected $ref = 1;
 
-    public function __construct($deg, $min, $sec) {
+    const N_REF = 1;
+    const S_REF = -1;
+    const E_REF = 1;
+    const O_REF = -1;
+
+    public function __construct($deg, $min, $sec, $ref) {
         $this->deg = $deg;
         $this->min = $min;
         $this->sec = $sec;
+        $this->ref = $ref;
     }
 
     public function getDecimal() {
-        return round(($this->deg) + ($this->min / 60) + ($this->sec / 3600), 6);
+        return $this->ref * round(($this->deg) + ($this->min / 60) + ($this->sec / 3600), 6);
     }
 
-    public static function fromArray(array $point) {
+    public static function refFromString(string $ref) {
+        switch ($ref) {
+            case 'N':
+                return self::N_REF;
+            case 'S':
+                return self::S_REF;
+            case 'E':
+                return self::E_REF;
+            case '0':
+                return self::O_REF;
+            default :
+                return 1;
+        }
+    }
+
+    public static function fromArray(array $point, string $ref = '') {
         $res1 = $res2 = $res3 = 0;
         if (isset($point[0])) {
             list($num, $div) = explode('/', $point[0]);
@@ -43,7 +65,7 @@ class Point {
             list($num, $div) = explode('/', $point[2]);
             $res3 = (int) $num / (int) $div;
         }
-        return new Point($res1, $res2, $res3);
+        return new Point($res1, $res2, $res3, static::refFromString($ref));
     }
 
 }
